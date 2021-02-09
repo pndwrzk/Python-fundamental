0e121b6be5895eb0ecb3cdc2f743da5bdd3f68f1
Rizki Pandiwa <68141232+pndwrzk@users.noreply.github.com>
2021-02-09 23:16:32 +0700

    Add files via upload

---
+++ VariabelDanTipeData.py
@@ -0,0 +1,25 @@
+# variabel pada python tidak perlu menyebukan tipe datanya
+# contoh  variabel : nama , nama_ panjang
+# python adalah bahasa yang dinamis ,sebuah variabel  bisa diubah tipe datanya
+
+harga = 1000
+print(harga)
+
+harga = harga * 3
+print(harga)
+
+
+print("================================")
+
+
+# tipe data basic pada python
+contoh_string = "Rizki Pandiwa"
+contoh_integer = 21
+contoh_float = 0.5
+contoh_boolean = True
+
+# mencetak tipe data pada sebuah variabel
+print(type(contoh_string))
+print(type(contoh_integer))
+print(type(contoh_float))
+print(type(contoh_boolean))

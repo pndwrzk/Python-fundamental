e52cb5cf0f32cb028539edf7f6b2ff70a5c90a16
Rizki Pandiwa <68141232+pndwrzk@users.noreply.github.com>
2021-02-09 23:15:23 +0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+# python dieksekusi berurutan dari atas kebawah
+# python bersifat case sensitif , yang artinya huruf kecil dan huruf besar dianggap beda
+
+# fungsi print adalah untuk mencetak
+# contoh penggunaan print
+print("Hello World")
+# print("Rizki Pandiwa") : Tercetak = Rizki Pandiwa dengan type data string
+print(100)
+# print(100) : Tercetak = 100 dengan type data integer
+print("Pandiwa " * 3)
+# print("Pandiwa " * 3) :tercetak = pandiwa pandiwa pandiwa , dengan type data string
